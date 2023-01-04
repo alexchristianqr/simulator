e0d75ace9b5114c40e6c74fbe80a4c89028c889b
Alex Christian <alexchristianqr@gmail.com>
2023-01-04 15:15:13 -0500

    [updated] app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container-fluid">
+    <AppSimulator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppSimulator from "@/components/AppSimulator";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppSimulator,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+/** Styles app */
+@import 'assets/scss/style.scss';
 </style>
---
+++ src/components/AppSimulator.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <h1>Hola depositos</h1>
+  <b-row>
+    <b-col cols="6">
+      <b-card title="Titulo 001">
+
+        <b-form>
+        <b-form-group label="Valor a invertir">
+          <b-form-input></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Plazo en meses">
+          <b-form-input></b-form-input>
+        </b-form-group>
+
+
+          <b-form-group label="Pago de intereses">
+            <b-form-select v-model="selected" :options="options"></b-form-select>
+          </b-form-group>
+        </b-form>
+
+      </b-card>
+
+    </b-col>
+    <b-col cols="6">
+      <b-card title="Titulo 002"></b-card>
+
+    </b-col>
+  </b-row>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AppSimulator",
+  data() {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: 'Please select some item' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Default Selected Option' },
+        { value: 'c', text: 'This is another option' },
+        { value: 'd', text: 'This one is disabled', disabled: true }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
